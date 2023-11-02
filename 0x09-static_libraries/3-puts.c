41eff3fc10788332dda8cb47f0c6da95b0e30447
Abikoye Oluwtomiwa <98543880+0xCiphera@users.noreply.github.com>
2023-11-01 19:05:52 -0700

    3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,17 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _puts - prints a string, followed by a newline to stdout
+ * @str: string parameter to print
+ * Return: Nothing
  */
-int main(void)
+
+void _puts(char *str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		++str;
+	}
+	_putchar('\n');
 }
